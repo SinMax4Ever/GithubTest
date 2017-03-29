ca170f62c70d596e8049ba7253d911f4565331de
MAX-PC\Max <s980704@gmail.com>
2017-03-30 00:03:36 +0800

    0329

--- GithubTest/app/src/main/java/com/s980704gmail/githubtest/Main2Activity.java
+++ GithubTest/app/src/main/java/com/s980704gmail/githubtest/Main2Activity.java
@@ -1,29 +1,26 @@
 package com.s980704gmail.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button nextPageBtn = (Button)findViewById(R.id.button2);
-        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main2);
+        Button backPageBtn = (Button)findViewById(R.id.button1);
+        backPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this,Main2Activity.class);
+                intent.setClass(Main2Activity.this,MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-
     }
-
-
 }
-
